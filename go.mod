b149b9b0de4616863afb157708421c07acb29214
Neil Wei <mongcheng@gmail.com>
2020-05-07 15:23:22 +0800

    Revmoe vendor

--- go.mod
+++ go.mod
@@ -7,8 +7,9 @@ require (
 	github.com/boj/redistore v0.0.0-20180917114910-cd5dcc76aeff // indirect
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/gin-contrib/cors v1.3.1
-	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607 // indirect
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
 	github.com/gin-gonic/gin v1.6.3
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.4.1 // indirect
 	github.com/gorilla/sessions v1.2.0 // indirect
 	github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path"
 	"time"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
 	"github.com/mong0520/go-google-photos-demo/handlers"
 	"github.com/zalando/gin-oauth2/google"
 	"golang.org/x/oauth2"
+	googleAuth "golang.org/x/oauth2/google"
 	"google.golang.org/api/photoslibrary/v1"
 )
 
 var redirectURL, credFile string
 var tokenCache map[string]*oauth2.Token
+var conf *oauth2.Config
+var state string
+var store sessions.CookieStore
+var cred google.Credentials
 
 type User struct {
 	Sub           string `json:"sub"`
@@ -63,16 +75,16 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 	// init settings for google auth
-	google.Setup(redirectURL, credFile, scopes, secret)
-	router.Use(google.Session(sessionName))
+	Setup(redirectURL, credFile, scopes, secret)
+	router.Use(Session(sessionName))
 
-	router.GET("/login", google.LoginHandler)
+	router.GET("/login", LoginHandler)
 	router.GET("/albums", handlers.AlbumsHandler)
 	router.Static("/web", "./web")
 
 	// protected url group
 	private := router.Group("/auth")
-	private.Use(google.Auth())
+	private.Use(Auth())
 	private.GET("/success", handlers.SuccessHandler)
 	// private.GET("/api", func(ctx *gin.Context) {
 	// 	token, err := ctx.Cookie("myphoto_cookie")
@@ -85,6 +97,91 @@ func main() {
 	// })
 
 	router.Run(":5000")
+}
+func Setup(redirectURL, credFile string, scopes []string, secret []byte) {
+	store = sessions.NewCookieStore(secret)
+	var c google.Credentials
+	file, err := ioutil.ReadFile(credFile)
+	if err != nil {
+		glog.Fatalf("[Gin-OAuth] File error: %v\n", err)
+	}
+	json.Unmarshal(file, &c)
+
+	conf = &oauth2.Config{
+		ClientID:     c.ClientID,
+		ClientSecret: c.ClientSecret,
+		RedirectURL:  redirectURL,
+		Scopes:       scopes,
+		Endpoint:     googleAuth.Endpoint,
+	}
+}
+
+func randToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func LoginHandler(ctx *gin.Context) {
+	state = randToken()
+	session := sessions.Default(ctx)
+	session.Set("state", state)
+	session.Save()
+	ctx.Writer.Write([]byte("<html><title>Golang Google</title> <body> <a href='" + GetLoginURL(state) + "'><button>Login with Google!</button> </a> </body></html>"))
+}
+
+func Auth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		// Handle the exchange code to initiate a transport.
+		session := sessions.Default(ctx)
+		fmt.Printf("sessino state: %s, query state: %s", session.Get("state"), ctx.Query("state"))
+		retrievedState := session.Get("state")
+		if retrievedState != ctx.Query("state") {
+			ctx.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
+			return
+		}
+
+		tok, err := conf.Exchange(oauth2.NoContext, ctx.Query("code"))
+		if err != nil {
+			ctx.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		client := conf.Client(oauth2.NoContext, tok)
+		email, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+		if err != nil {
+			ctx.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+		defer email.Body.Close()
+		data, err := ioutil.ReadAll(email.Body)
+		if err != nil {
+			glog.Errorf("[Gin-OAuth] Could not read Body: %s", err)
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		var user User
+		err = json.Unmarshal(data, &user)
+		if err != nil {
+			glog.Errorf("[Gin-OAuth] Unmarshal userinfo failed: %s", err)
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		// save userinfo, which could be used in Handlers
+		ctx.Set("user", user)
+		ctx.Set("conf", conf)
+		ctx.Set("token", tok)
+		ctx.Set("client", client)
+	}
+}
+
+func GetLoginURL(state string) string {
+	return conf.AuthCodeURL(state)
+}
+
+func Session(name string) gin.HandlerFunc {
+	return sessions.Sessions(name, store)
 }
 
 // func ShowUserInfo(ctx *gin.Context) {
